2b6c2c903c29bd4617f76d402074142439253f2a
Lucas Moidel <lucas122133@gmail.com>
2026-02-13 15:22:14 -0800

    close

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ def charToInt(char):
 def intToChar(num):
     letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     return letters[num]
+        
 
-rotor1 = [4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9]
-rotor2 = [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]
-rotor3 = [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14]
+rotor1 = [4, 10, 12, 5, 11, 6, 3, 16, 21, 26, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9]
+rotor2 = [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 26, 13, 15, 24, 5, 21, 14, 4]
+rotor3 = [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 26, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14]
 
-reflector = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
+reflector = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 26, 2, 22, 21, 9, 0, 19]
 
 rotor1offset = charToInt("a")
-rotor2offset = charToInt("a")
-rotor3offset = charToInt("a")
+rotor2offset = charToInt("e")
+rotor3offset = charToInt("v")
 
 
 string = input("type your message here: ").lower()
@@ -24,24 +25,31 @@ for i in string:
     if i == " ":
         result+=" "
     else:
-        rotor3offset += 1;
-        rotor3offset = rotor3offset % 25
-        x = charToInt(i)+rotor3offset
-        print(intToChar(x))
-        c = rotor3[x]-rotor3offset
-        print(intToChar(c))
-        c = rotor2[c]
-        print(intToChar(c))
-        c = rotor1[c]
-        print(intToChar(c))
+        rotor3offset = (rotor3offset + 1) % 26
+        if(rotor3offset == charToInt("w")):
+            rotor2offset = (rotor2offset + 1) % 26
+        if(rotor2offset == charToInt("f")):
+            rotor1offset = (rotor1offset + 1) % 26
+        c = (charToInt(i) + rotor3offset) % 26
+        c = (rotor3[c] - rotor3offset) % 26
+
+        c = (c + rotor2offset) % 26
+        c = (rotor2[c] - rotor2offset) % 26
+
+        c = (c + rotor1offset) % 26
+        c = (rotor1[c] - rotor1offset) % 26
+
         c = reflector[c]
-        print(intToChar(c))
-        c = rotor1.index(c)
-        print(intToChar(c))
-        c = rotor2.index(c)
-        print(intToChar(c))
-        c = rotor3.index(c-rotor3offset)
-        print(intToChar(c))
+        
+        c = rotor1.index((c + rotor1offset) % 26)
+        c = (c - rotor1offset) % 26
+        
+        c = rotor2.index((c + rotor2offset) % 26)
+        c = (c - rotor2offset) % 26
+        
+        c = rotor3.index((c + rotor3offset) % 26)
+        c = (c - rotor3offset) % 26
+        
         result += intToChar(c)
 
 
