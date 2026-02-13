cd2c1285855ab1d6b956c05dc6efddab8390799d
Lucas Moidel <lucas122133@gmail.com>
2026-02-13 00:58:20 -0800

    progress

--- main.py
+++ main.py
@@ -6,23 +6,29 @@ def charToInt(char):
 def intToChar(num):
     letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
     return letters[num]
+
 rotor1 = [4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9]
 rotor2 = [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]
 rotor3 = [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14]
 
 reflector = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
-# notch3 = rotor3.index(charToInt("v"))
-# notch2 = rotor2.index(charToInt("e"))
-# notch1 = rotor1.index(charToInt("q"))
 
-string= input("type your message here: ").lower()
+rotor1offset = charToInt("a")
+rotor2offset = charToInt("a")
+rotor3offset = charToInt("a")
+
+
+string = input("type your message here: ").lower()
 result = ""
 for i in string:
     if i == " ":
         result+=" "
     else:
-        x = charToInt(i)
-        c = rotor3[x]
+        rotor3offset += 1;
+        rotor3offset = rotor3offset % 25
+        x = charToInt(i)+rotor3offset
+        print(intToChar(x))
+        c = rotor3[x]-rotor3offset
         print(intToChar(c))
         c = rotor2[c]
         print(intToChar(c))
@@ -34,11 +40,9 @@ for i in string:
         print(intToChar(c))
         c = rotor2.index(c)
         print(intToChar(c))
-        c = rotor3.index(c)
+        c = rotor3.index(c-rotor3offset)
         print(intToChar(c))
         result += intToChar(c)
-
-
 
 
 
