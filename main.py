a10f82be399aacc384a946493973a17e73a62fb3
Lucas Moidel <lucas122133@gmail.com>
2026-02-14 16:00:14 -0800

    inputs

--- main.py
+++ main.py
@@ -8,54 +8,99 @@ def intToChar(num):
     return letters[num]
         
 
+rotors = [[], [], [], []]
+offsets = [charToInt("a"), charToInt("a"), charToInt("a")]
+
 rotor1 = [4, 10, 12, 5, 11, 6, 3, 16, 21, 25, 13, 19, 14, 22, 24, 7, 23, 20, 18, 15, 0, 8, 1, 17, 2, 9]
 rotor2 = [0, 9, 3, 10, 18, 8, 17, 20, 23, 1, 11, 7, 22, 19, 12, 2, 16, 6, 25, 13, 15, 24, 5, 21, 14, 4]
 rotor3 = [1, 3, 5, 7, 9, 11, 2, 15, 17, 19, 23, 21, 25, 13, 24, 4, 8, 22, 6, 0, 10, 12, 20, 18, 16, 14]
 
-reflector = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
+reflectora = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
+reflectorb = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
+reflectorc = [24, 17, 20, 7, 16, 18, 11, 3, 15, 23, 13, 6, 14, 10, 12, 8, 4, 1, 5, 25, 2, 22, 21, 9, 0, 19]
 
-rotor1offset = charToInt("a")
-rotor2offset = charToInt("a")
-rotor3offset = charToInt("a")
+ 
+while len(rotors[0]) == 0:
+    ref = input("Reflector A, B, or C: ")
+    if ref.lower() == "a":
+        rotors[0] = reflectora
+    elif ref.lower() == "b":
+        rotors[0] = reflectorb
+    elif ref.lower() == "c":
+        rotors[0] = reflectorc
+
+while len(rotors[1]) == 0:
+    ref = input("Rotor 1, 2, or 3: ")
+    if ref.lower() == "1":
+        rotors[1] = rotor1
+    elif ref.lower() == "2":
+        rotors[1] = rotor2
+    elif ref.lower() == "3":
+        rotors[1] = rotor3
+    
+while len(rotors[2]) == 0:
+    ref = input("Rotor 1, 2, or 3: ")
+    if ref.lower() == "1":
+        rotors[2] = rotor1
+    elif ref.lower() == "2":
+        rotors[2] = rotor2
+    elif ref.lower() == "3":
+        rotors[2] = rotor3
+
+while len(rotors[3]) == 0:
+    ref = input("Rotor 1, 2, or 3: ")
+    if ref.lower() == "1":
+        rotors[3] = rotor1
+    elif ref.lower() == "2":
+        rotors[3] = rotor2
+    elif ref.lower() == "3":
+        rotors[3] = rotor3
+
+def setoffset():
+    ref = input("Offset letters \"XXX\": ")
+    for i in range(len(ref)):
+        if ref[i].isalpha:
+            offsets[i] = charToInt(ref[i])
+        else: 
+            print("try again")
+            setoffset()
+
+setoffset()
 
 
 string = input("type your message here: ").lower()
 result = ""
+
 for i in string:
     if i == " ":
         result+=" "
     else:
-        rotor3offset = (rotor3offset + 1) % 26
-        if(rotor3offset == charToInt("w")):
-            if(rotor2offset == charToInt("e")):
-                rotor1offset = (rotor1offset + 1) % 26
-            rotor2offset = (rotor2offset + 1) % 26
+        offsets[2] = (offsets[2] + 1) % 26
+        if(offsets[2] == charToInt("w")):
+            if(offsets[1] == charToInt("e")):
+                offsets[0] = (offsets[0] + 1) % 26
+            offsets[1] = (offsets[1] + 1) % 26
 
 
-        print(intToChar(rotor1offset) + " " + intToChar(rotor2offset) + " " + intToChar(rotor3offset))
+        c = (charToInt(i) + offsets[2]) % 26
+        c = (rotors[3][c] - offsets[2]) % 26
 
+        c = (c + offsets[1]) % 26
+        c = (rotors[2][c] - offsets[1]) % 26
 
-        c = (charToInt(i) + rotor3offset) % 26
-        c = (rotor3[c] - rotor3offset) % 26
+        c = (c + offsets[0]) % 26
+        c = (rotors[1][c] - offsets[0]) % 26
 
-        c = (c + rotor2offset) % 26
-        c = (rotor2[c] - rotor2offset) % 26
-
-        c = (c + rotor1offset) % 26
-        c = (rotor1[c] - rotor1offset) % 26
-
-        c = reflector[c]
+        c = rotors[0][c]
         
-        c = rotor1.index((c + rotor1offset) % 26)
-        c = (c - rotor1offset) % 26
+        c = rotors[1].index((c + offsets[0]) % 26)
+        c = (c - offsets[0]) % 26
         
-        c = rotor2.index((c + rotor2offset) % 26)
-        c = (c - rotor2offset) % 26
+        c = rotors[2].index((c + offsets[1]) % 26)
+        c = (c - offsets[1]) % 26
         
-        print((c + rotor3offset) % 26)
-
-        c = rotor3.index((c + rotor3offset) % 26)
-        c = (c - rotor3offset) % 26
+        c = rotors[3].index((c + offsets[2]) % 26)
+        c = (c - offsets[2]) % 26
         
         result += intToChar(c)
 
